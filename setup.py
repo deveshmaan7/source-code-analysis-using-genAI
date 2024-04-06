755f47679fc4722a0bcaec305b586f4584cb4fe8
deveshmaan7 <deveshmaan7@gmail.com>
2024-04-06 20:09:06 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Source Code analysis project',
+    version= '0.0.0',
+    author= 'Devesh Maan',
+    author_email= 'deveshmaan7@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
